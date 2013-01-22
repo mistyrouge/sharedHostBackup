4886dc621646d3c75364f270ee10d3ece746e87b
Dorian JAMINAIS <dorian@jaminais.fr>
2013-01-22 09:59:52 -0500

    dumpdb.php is now uploaded by the backup script

--- backup.sh
+++ backup.sh
@@ -5,9 +5,31 @@ USER="username"
 PASS="password"
 LCD="/path/of/your/local/dir"
 RCD="/path/of/your/remote/dir"
+NOW=$(date +"%Y-%m-%d")
 
-curl http://@HOST/dumpdb.php
+##
+# First put the the php script on the server
+##
+ftp -inv $HOST << EOF
+user $USER $PASS
+cd $RCD
+put dumpdb.php
+bye
+EOF 
 
+##
+# Dump the database to db-backup.sql
+##
+curl http://$HOST/dumpdb.php
+
+##
+# Create needed directories
+##
+mkdir $LCD
+
+##
+# Download all the files from the ftp
+##
 lftp -c "set ftp:list-options -a;
 open ftp://$USER:$PASS@$HOST; 
 lcd $LCD;
@@ -15,3 +37,22 @@ cd $RCD;
 mirror --reverse \
        --delete \
        --verbose"
+
+##
+# remove the database dump and php script form the server
+##
+ftp -inv $HOST << EOF
+user $USER $PASS
+cd $RCD
+delete dumpdb.php
+delete db-backup.sql
+bye
+EOF 
+
+##
+# commit changes to git
+##
+cd $LCD
+git commit -a -m $NOW
+
+echo "--- The END ---"
